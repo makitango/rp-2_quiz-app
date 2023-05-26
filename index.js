63aecde5479a1ecb8261a840c0f12bbe9dbe7661
Dominic Bergmann <dominic.bergmann@gmail.com>
2023-05-26 15:25:51 +0200

    initial commit

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ const bookmarkButton = document.querySelector('[data-js="bookmarkIcon"]');
 bookmarkButton.addEventListener("click", () => {
   bookmarkButton.classList.toggle("bookmark--active");
 });
+
+const cardAnswerButton = document.querySelector('[data-js="cardAnswerButton"]');
+const cardAnswer = document.querySelector('[data-js="cardAnswer"]');
+
+cardAnswerButton.addEventListener("click", () => {
+  cardAnswerButton.classList.toggle("card__answer--active");
+  cardAnswer.classList.toggle("card__answer");
+
+  if (cardAnswerButton.textContent === "Hide answer") {
+    cardAnswerButton.textContent = "Show answer";
+  } else {
+    cardAnswerButton.textContent = "Hide answer";
+  }
+});
