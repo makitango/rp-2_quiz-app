1889257aec1775a01f9f31459a7a2c2328f7e6a1
Dominic Bergmann <dominic.bergmann@gmail.com>
2023-05-26 11:41:23 +0200

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // phew… not a lot going on here. Please add some code!
+const bookmarkButton = document.querySelector('[data-js="bookmarkIcon"]');
+console.log(bookmarkButton);
+
+bookmarkButton.addEventListener("click", () => {
+  bookmarkButton.classList.toggle("bookmark--active");
+});
