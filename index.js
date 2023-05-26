cea00ccd7a0300bc5b9976285778345dc1916a23
Dominic Bergmann <dominic.bergmann@gmail.com>
2023-05-26 12:15:45 +0200

    removed console.log

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // phew… not a lot going on here. Please add some code!
 const bookmarkButton = document.querySelector('[data-js="bookmarkIcon"]');
-console.log(bookmarkButton);
 
 bookmarkButton.addEventListener("click", () => {
   bookmarkButton.classList.toggle("bookmark--active");
